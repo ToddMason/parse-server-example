385dddc86ed091b157f91c207b88d54ca74e444d
ToddMason <toddmason@icloud.com>
2016-03-11 10:46:36 -0600

    Update main.js
    more dang test code. crossing fingers.

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,10 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi Todd Mason!');
 });
 
+Parse.Cloud.afterSave(“TestClass”, function(request) {   
+   console.log(“We saved some data!”);	
+
+});
 
 
 
