742e3bc8b298ae99195bb4b03cd11391aa171eec
ToddMason <toddmason@icloud.com>
2016-03-11 10:12:39 -0600

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,16 @@
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi Todd Mason!');
-});
+}
+
+Parse.Cloud.beforeSave(“TestClass”, function(request, response) {
+  if (request.object.get(“phone”) = 0) {
+    response.error(“phone number required”);
+  } 
+  else {
+    response.success();
+  }
+}
+
+);
 
